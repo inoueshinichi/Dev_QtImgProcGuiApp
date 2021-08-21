eaece359bbd1b5689649e09c3c7bd8a7d37b6530
Inoue Shinichi for Windows Desktop <inoue.shinichi.1800@gmail.com>
2021-08-21 13:19:12 +0900

    軽微な修正

--- qt_gui_app/image_window.cpp
+++ qt_gui_app/image_window.cpp
@@ -2,11 +2,12 @@
 #include "image_window.h"
 #include "main_window.h"
 
+#include <ctime>
 #include <string>
 #include <iostream>
 
-ImageWindow::ImageWindow(QWidget *parent)
-    : QMainWindow(parent)
+ImageWindow::ImageWindow(QWidget *p_parent)
+    : QMainWindow(p_parent)
     , m_pUi(new Ui::ImageWindow())
     , m_pStatusBarLabel(new QLabel())
 {
@@ -42,6 +43,38 @@ void ImageWindow::customConnection()
     // connect(this, &ImageWindow::activeImgWin, 
     //     (MainWindow *)(this->parent()), &MainWindow::slotActiveImgWin);
 }
+
+void ImageWindow::setFilename(const QString& filename)
+{
+    /*ファイル名 + 時間*/
+
+    // 時間取得
+    char timeString[256];
+    std::memset((void *)&timeString, '\0', sizeof(timeString));
+    time_t theTime = time(nullptr);
+    struct tm *stm = localtime(&theTime);
+    if (theTime == -1 || stm == nullptr)
+    {
+        std::snprintf(timeString, 256, "Unknown");
+    }
+    else
+    {
+        strftime(timeString, 256, "%Y-%m-%d %H:%M ", stm);
+    }
+    QString timeNow(timeString);
+    this->setWindowTitle(timeNow + filename);
+    m_filename = filename;
+}
+
+QString ImageWindow::filename() const { return m_filename; }
+
+
+void ImageWindow::toggleCrossLine(bool isShow)
+{
+    /*マウスの十字線の表示切り替え*/
+    m_pScene->toggleCrossLine(isShow);
+}
+
 
 void ImageWindow::closeEvent(QCloseEvent *event)
 {
