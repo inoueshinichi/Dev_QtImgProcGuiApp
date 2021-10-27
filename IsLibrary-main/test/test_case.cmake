fe320b9bea864975b50ce40b0fa15b5579e4ca4a
Inoue Shinichi XPS13 9310 on Ubuntu 20.04 LTS <inoue.shinichi.1800@gmail.com>
2021-10-28 01:28:49 +0900

    [Update] テストケースにPython, Numpyを入れるとOpenCVのTiffでコンフリクトする

--- IsLibrary-main/test/test_case.cmake
+++ IsLibrary-main/test/test_case.cmake
@@ -3,7 +3,7 @@ cmake_minimum_required(VERSION 3.14.6)
 
 function(make_test_case SOURCE_CODE TEST_TARGET_EXE)
 
-    find_package(Python3 COMPONENTS Development NumPy)
+    # find_package(Python3 COMPONENTS Development NumPy)
 
     # Exe file
     add_executable(${TEST_TARGET_EXE}
@@ -23,8 +23,8 @@ function(make_test_case SOURCE_CODE TEST_TARGET_EXE)
         ${IS_COMPUTERVISION_TARGET_NAME}
         GTest::gtest
         GTest::gmock
-        Python3::Python
-        Python3::NumPy
+        # Python3::Python
+        # Python3::NumPy
     )
 
     # Build-order
@@ -42,5 +42,18 @@ function(make_test_case SOURCE_CODE TEST_TARGET_EXE)
         COMMAND ${CMAKE_COMMAND} -E copy_if_different $<TARGET_FILE:${IS_NDARRAY_TARGET_NAME}> $<TARGET_FILE_DIR:${TEST_TARGET_EXE}>
         COMMAND ${CMAKE_COMMAND} -E copy_if_different $<TARGET_FILE:${IS_COMPUTERVISION_TARGET_NAME}> $<TARGET_FILE_DIR:${TEST_TARGET_EXE}>
     )
+
+    # ###############################################################################
+    # # OpenCVライブラリのリンク
+    # ###############################################################################
+    # find_package(OpenCV REQUIRED)
+    # if(OpenCV_FOUND)
+    #     target_include_directories(${TEST_TARGET_EXE} PUBLIC 
+    #         ${OpenCV_INCLUDE_DIRS}
+    #     )
+    #     target_link_libraries(${TEST_TARGET_EXE} PUBLIC 
+    #         ${OpenCV_LIBS}
+    #     )
+    # endif()
 
 endfunction()
