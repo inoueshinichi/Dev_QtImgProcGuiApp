d7b8f542bca3c34ad52e63df49e52181dc69b476
Inoue Shinichi XPS13 9310 on Ubuntu 20.04 LTS <inoue.shinichi.1800@gmail.com>
2021-10-28 01:28:01 +0900

    [Update] 軽微な修正

--- IsLibrary-main/src/IsComputerVision/filter/blur.cpp
+++ IsLibrary-main/src/IsComputerVision/filter/blur.cpp
@@ -9,7 +9,7 @@ namespace is
         using uchar = unsigned char;
 
 
-        NdArrayPtr avg_filter(NdArrayPtr src, const Size &ksize)
+        NdArrayPtr avg_filter(NdArrayPtr src, const Size& ksize)
         {
             IS_CHECK_NDARRAY_SHAPE_AS_IMAGE(src);
             IS_DEBUG_CHECK_NDARRAY_STATE(avg_filter, IS_DEBUG_FLAG, src);
@@ -116,6 +116,7 @@ namespace is
             return dst;
         }
 
+
         NdArrayPtr median_filter(NdArrayPtr src, int ksize)
         {
             IS_CHECK_NDARRAY_SHAPE_AS_IMAGE(src);
@@ -146,92 +147,100 @@ namespace is
             ubyte *dst_data = dst->cast_data_and_get_pointer<ubyte>(ctx);
 
             /*チャネル毎のヒストグラム*/
-            // int *p_hist256 = new int[256];
-            // IS_ZERO_MEMORY(p_hist256, sizeof(int) * 256); // 1
-            // int *p_hist128 = new int[128];
-            // IS_ZERO_MEMORY(p_hist128, sizeof(int) * 128); // 2
-            // int *p_hist64 = new int[64];
-            // IS_ZERO_MEMORY(p_hist64, sizeof(int) * 64);   // 4
-            // int *p_hist32 = new int[32];
-            // IS_ZERO_MEMORY(p_hist32, sizeof(int) * 32);   // 8
-            // int *p_hist16 = new int[16];
-            // IS_ZERO_MEMORY(p_hist16, sizeof(int) * 16);   // 16
-            // int *p_hist8 = new int[8];
-            // IS_ZERO_MEMORY(p_hist8, sizeof(int) * 8);     // 32
-            // int *p_hist4 = new int[4];
-            // IS_ZERO_MEMORY(p_hist4, sizeof(int) * 4);     // 64
-            // int *p_hist2 = new int[2];
-            // IS_ZERO_MEMORY(p_hist2, sizeof(int) * 2);     // 128
-            // int *p_hist1 = new int[1];
-            // IS_ZERO_MEMORY(p_hist1, sizeof(int) * 1);     // 256
-            int **pp_hists = new int*[9];
-            int *p_bins = new int[9];
-            int *p_sums = new int[9];
-            int *p_heidx = new int[9];
-            int range = 256;
-            for (int i = 0; i < 9; ++i, range /= 2) {
-                pp_hists[i] = new int[range];
-                IS_ZERO_MEMORY(pp_hists[i], sizeof(int) * range);
-                p_bins[i] = 256 / range;
-                p_sums[i] = p_bins[i];
-                p_heidx[i] = 0;
+            // 256, 128, 64, 32, 16, 8, 4, 2, (1) 
+            const int HIST_NUM = 8;
+            int **pp_hists = new int*[HIST_NUM];
+            int *p_bins = new int[HIST_NUM];
+            for (int k = 0, range = 256; k < HIST_NUM; ++k, range /= 2) {
+                pp_hists[k] = new int[range];
+                IS_ZERO_MEMORY(pp_hists[k], sizeof(int) * range);
+                p_bins[k] = 256 / range;
             }
-            int *p_hist256 = pp_hists[0];
+    
 
             /*ヒストグラムを用いて中央値を求める*/
-            int mid = (ksize * ksize / 2) + 1; // 1スタート 3x3=9個なら1,2,3,4,5,6,7,8,9の5番
+            // https://qiita.com/YVT/items/7734b342894c2f8247b3
 
+            int mid = (ksize * ksize / 2) + 1; // 1スタート 3x3=9個なら1,2,3,4,5,6,7,8,9の5番
             for (int c = 0; c < channels; ++c) {
               for (int y = 0, ex_y = hlf_ks; y < height; ++y, ++ex_y) {
                 for (int x = 0, ex_x = hlf_ks; x < width; ++x, ++ex_x) {
                     
                     if (x == 0) {
+                        
                         // ヒストグラム初期化
-                        IS_ZERO_MEMORY(p_hist256, sizeof(int) * 256);
+                        for (int k = 0, range = 256; k < HIST_NUM; ++k, range /= 2) {
+                            IS_ZERO_MEMORY(pp_hists[k], sizeof(int) * range);
+                        }
+
+                        // 階層bin別のヒストグラムを求める
                         for (int j = 0; j < ksize; ++j) {
                             for (int i = 0; i < ksize; ++i) {
                                 const auto& lum = ex_data[c * exst[0] + (y + j) * exst[1] + i * exst[2]];
-                                p_hist256[lum] += 1;
+                                for (int k = 0; k < HIST_NUM; ++k) {
+                                    pp_hists[k][lum / p_bins[k]] += 1;
+                                }
                             }
                         }
+
                     }
                     else {
                         // 減らす
                         int l = x - 1;
                         for (int j = 0; j < ksize; ++j) {
-                            const auto& lum = ex_data[c * exst[0] + (y + j) * exst[1] + l];
-                            p_hist256[lum] -= 1;
+                            const auto& lum = ex_data[c * exst[0] + (y + j) * exst[1] + l * exst[2]];
+
+                            // 各階層のhist
+                            for (int k = 0; k < HIST_NUM; ++k) {
+                                int index = lum / p_bins[k];
+                                pp_hists[k][index] -= 1;
+                            }
                         }
                         // 増やす
                         int r = l + ksize;
                         for (int j = 0; j < ksize; ++j) {
-                            const auto& lum = ex_data[c * exst[0] + (y + j * exst[1] + r)];
-                            p_hist256[lum] += 1;
-                        }
-                    }
+                            const auto& lum = ex_data[c * exst[0] + (y + j) * exst[1] + r * exst[2]];
 
-                    // 階層bin別のヒストグラムを求める
-                    for (; p_heidx[0] < 256; p_heidx[0] += 1) {
-                        for (int i = 1; i < 9; ++i) {
-                            if (p_heidx[0] > p_sums[i]) {
-                                p_sums[i] += p_bins[i];
-                                p_heidx[i] += 1;
+                            // 各階層のhist
+                            for (int k = 0; k < HIST_NUM; ++k) {
+                                int index = lum / p_bins[k];
+                                pp_hists[k][index] += 1;
                             }
-                            pp_hists[i][p_heidx[i]] += pp_hists[0][p_heidx[0]];
                         }
                     }
 
-
+                    
+                    // binの大きなヒストグラムから辿ってメディアンな輝度を求める.
+                    ubyte index; // median lum
+                    ubyte first, last, sum_hist;
+                    int hierarchy_idx = HIST_NUM - 1;
+                    first = 0; last = 256 / p_bins[hierarchy_idx];
+                    sum_hist = 0; // 累積ヒストグラム
+                    while (hierarchy_idx >= 0) {
+                        for (index = first; index < last; ++index) {
+                            int judge_sum_hist = sum_hist + pp_hists[hierarchy_idx][index];
+                            if (mid <= judge_sum_hist) {
+                                first = 2 * index;
+                                last = 2 * (index + 1);
+                                break;
+                            }
+                            else {
+                                sum_hist += pp_hists[hierarchy_idx][index];
+                            }
+                            
+                        }
+                        hierarchy_idx--;
+                    }
+                    
+                    dst_data[c * st[0] + y * st[1] + x * st[2]] = index;
                     
                 }
               }
             }
 
             // メモリ破棄
-            delete[] p_heidx; p_heidx = nullptr;
-            delete[] p_sums; p_sums = nullptr;
             delete[] p_bins; p_bins = nullptr;
-            for (int i = 0; i < 9; ++i) {
+            for (int i = 0; i < HIST_NUM; ++i) {
                 delete[] pp_hists[i]; pp_hists[i] = nullptr;
             }
             delete[] pp_hists; pp_hists = nullptr;
