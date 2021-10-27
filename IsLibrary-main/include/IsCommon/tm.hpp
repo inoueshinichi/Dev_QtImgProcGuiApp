bd7d210b4e572047a2c11f83b5c0128c62336325
Inoue Shinichi XPS13 9310 on Ubuntu 20.04 LTS <inoue.shinichi.1800@gmail.com>
2021-10-28 01:27:21 +0900

    [Doing] 処理時間の前に関数名を自動で追加できるように実装中

--- IsLibrary-main/include/IsCommon/tm.hpp
+++ IsLibrary-main/include/IsCommon/tm.hpp
@@ -18,6 +18,8 @@
 #endif
 #include <iostream>
 
+#define IS_STR_FUNCNAME(func_name) #func_name
+
 namespace is {
     namespace common {
 
@@ -26,22 +28,20 @@ namespace is {
          * 単位: [ms]
          */
         auto invoke_tm_c_ms = [](auto &&func, auto &&...args) -> double {
-            std::cout << "C clock(): ";
             std::clock_t start = std::clock();
             std::forward<decltype(func)>(func)(std::forward<decltype(args)>(args)...); // 完全転送
             std::clock_t end = std::clock();
             std::size_t duration = (double)(end - start);
-            std::cout << duration << "[ms]" << std::endl;
+            std::cout << IS_STR_FUNCNAME(func) << ": " << duration << "[ms]" << std::endl;
             return duration;
         };
 
         auto invoke_tm_c_ms_ret = [](auto &&func, auto &&...args) -> auto {
-            std::cout << "C clock(): ";
             std::clock_t start = std::clock();
             auto result = std::forward<decltype(func)>(func)(std::forward<decltype(args)>(args)...); // 完全転送
             std::clock_t end = std::clock();
             std::size_t duration = (double)(end - start);
-            std::cout << duration << "[ms]" << std::endl;
+            std::cout << IS_STR_FUNCNAME(func) << ": " << duration << "[ms]" << std::endl;
             return result;
         };
 
@@ -50,13 +50,12 @@ namespace is {
          * 
          */
         auto invoke_tm_chrono_ms = [](auto &&func, auto &&...args) -> double {
-            std::cout << "std::chrono(): ";
             auto start = std::chrono::system_clock::now();                             // system_clock::now()
             std::forward<decltype(func)>(func)(std::forward<decltype(args)>(args)...); // 完全転送
             auto end = std::chrono::system_clock::now();
             auto duration = end - start;
             double duration_ms = std::chrono::duration_cast<std::chrono::milliseconds>(duration).count();
-            std::cout << duration_ms << "[ms]" << std::endl;
+            std::cout << IS_STR_FUNCNAME(func) << ": " << duration_ms << "[ms]" << std::endl;
             return duration_ms;
         };
 
@@ -65,13 +64,12 @@ namespace is {
          * 
          */
         auto invoke_tm_chrono_ms_ret = [](auto &&func, auto &&...args) -> auto {
-            std::cout << "std::chrono(): ";
             auto start = std::chrono::system_clock::now();                                           // system_clock::now()
             auto result = std::forward<decltype(func)>(func)(std::forward<decltype(args)>(args)...); // 完全転送
             auto end = std::chrono::system_clock::now();
             auto duration = end - start;
             double duration_ms = std::chrono::duration_cast<std::chrono::milliseconds>(duration).count();
-            std::cout << duration_ms << "[ms]" << std::endl;
+            std::cout << IS_STR_FUNCNAME(func) << ": " << duration_ms << "[ms]" << std::endl;
             return result;
         };
 
@@ -80,13 +78,12 @@ namespace is {
          * 
          */
         auto invoke_tm_chrono_ns = [](auto &&func, auto &&...args) -> double {
-            std::cout << "std::chrono(): ";
             auto start = std::chrono::high_resolution_clock::now();
             std::forward<decltype(func)>(func)(std::forward<decltype(args)>(args)...); // 完全転送
             auto end = std::chrono::high_resolution_clock::now();
             auto duration = end - start;
             double duration_ns = std::chrono::duration_cast<std::chrono::nanoseconds>(duration).count();
-            std::cout << duration_ns << "[ns]" << std::endl;
+            std::cout << IS_STR_FUNCNAME(func) << ": " << duration_ns << "[ns]" << std::endl;
             return duration_ns;
         };
 
@@ -95,13 +92,12 @@ namespace is {
          * 
          */
         auto invoke_tm_chrono_ns_ret = [](auto &&func, auto &&...args) -> auto {
-            std::cout << "std::chrono(): ";
             auto start = std::chrono::high_resolution_clock::now();
             auto result = std::forward<decltype(func)>(func)(std::forward<decltype(args)>(args)...); // 完全転送
             auto end = std::chrono::high_resolution_clock::now();
             auto duration = end - start;
             double duration_ns = std::chrono::duration_cast<std::chrono::nanoseconds>(duration).count();
-            std::cout << duration_ns << "[ns]" << std::endl;
+            std::cout << IS_STR_FUNCNAME(func) << ": " << duration_ns << "[ns]" << std::endl;
             return result;
         };
 
@@ -111,12 +107,12 @@ namespace is {
          * 単位: [ms]
          */
         auto invoke_tm_win32_1 = [](auto &&func, auto &&...args) -> double {
-            std::cout << "Win32 GetTickCount64(): ";
             ULONGLONG start = ::GetTickCount64();
             std::forward<decltype(func)>(func)(std::forward<decltype(args)>(args)...); // 完全転送
             ULONGLONG end = ::GetTickCount64();
             double duration = (double)(end - start);
-            std::cout << duration << "[ms]" << std::endl;
+            std::cout << "Win32 GetTickCount64(): ";
+            std::cout << IS_STR_FUNCNAME(func) << ": " << duration << "[ms]" << std::endl;
             return duration;
         };
 
@@ -125,12 +121,12 @@ namespace is {
          * 
          */
         auto invoke_tm_win32_2 = [](auto &&func, auto &&...args) -> double {
-            std::cout << "Win32 timeGetTime(): ";
             DWORD start = ::timeGetTime();
             std::forward<decltype(func)>(func)(std::forward<decltype(args)>(args)...); // 完全転送
             DWORD end = ::timeGetTime();
             double duration = (double)(end - start);
-            std::cout << duration << "[ms]" << std::endl;
+            std::cout << "Win32 timeGetTime(): ";
+            std::cout << IS_STR_FUNCNAME(func) << ": " << duration << "[ms]" << std::endl;
             return duration;
         };
 
@@ -139,15 +135,16 @@ namespace is {
          * 
          */
         auto invoke_tm_win32_3 = [](auto &&func, auto &&...args) -> double {
-            std::cout << "Win32 QueryPerformanceCounter(): ";
             LARGE_INTEGER freq;
             if (!(::QueryPerformanceFrequency(&freq))) {
+                std::cout << "Win32 QueryPerformanceCounter(): ";
                 std::cout << "fail to get time unit." << std::endl;
                 return 0;
             }
 
             LARGE_INTEGER start, end;
             if (!::QueryPerformanceFrequency(&start)) {
+                std::cout << "Win32 QueryPerformanceCounter(): ";
                 std::cout << "fail to start." << std::endl;
                 return 0;
             }
@@ -155,12 +152,14 @@ namespace is {
             std::forward<decltype(func)>(func)(std::forward<decltype(args)>(args)...); // 完全転送
 
             if (!::QueryPerformanceFrequency(&end)) {
+                std::cout << "Win32 QueryPerformanceCounter(): ";
                 std::cout << "fail to end." << std::endl;
                 return 0;
             }
 
             double duration = (double)(end.QuadPart - start.QuadPart) / freq.QuadPart;
-            std::cout << duration << "[ms]" << std::endl;
+            std::cout << "Win32 QueryPerformanceCounter(): ";
+            std::cout << IS_STR_FUNCNAME(func) << ": " << duration << "[ms]" << std::endl;
             return duration;
         };
 #endif
