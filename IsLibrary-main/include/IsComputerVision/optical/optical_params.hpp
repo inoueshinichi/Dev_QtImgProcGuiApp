e797a89a6ae999c34aa6a1b1900b1555a96d7bd2
Inoue Shinichi XPS13 9310 on Ubuntu 20.04 LTS <inoue.shinichi.1800@gmail.com>
2021-11-02 01:33:40 +0900

    [Init] 光学パラメータ

---
+++ IsLibrary-main/include/IsComputerVision/optical/optical_params.hpp
@@ -0,0 +1,118 @@
+/**
+ * @file optical_params.hpp
+ * @author your name (you@domain.com)
+ * @brief 光学パラメータの計算
+ * @version 0.1
+ * @date 2021-11-02
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+#pragma once
+
+#include <IsComputerVision/common.hpp>
+
+#include <numbers>
+#include <tuple>
+
+
+namespace is {
+    namespace imgproc {
+
+
+        /**
+         * @brief 許容錯乱円
+         * 
+         * @param distance 
+         * @return double 
+         */
+        inline double permissible_circle_of_confusion(double distance) {
+            return distance / 1500.0;
+        }
+
+
+        /**
+         * @brief 35mmフィルム換算の焦点距離
+         * 
+         * @param focus_length 
+         * @param diag_distance 
+         * @return double 
+         */
+        inline double cvt_35mm_focus_length(double focus_length, double diag_distance) {
+            return 43.3 * focus_length / diag_distance;
+        }
+
+
+        /**
+         * @brief レンズカテゴリ
+         * 
+         * @param focus_length_of_35mm 
+         * @return std::string 
+         */
+        inline std::string lens_category(double focus_length_of_35mm) {
+            // 0~40mm:広角レンズ
+            // 40~100mm:標準レンズ
+            // 100mm~:望遠レンズ
+	        std::string category;
+	        if (focus_length_of_35mm < 40) category = "広角レンズ";
+	        else if (focus_length_of_35mm > 100) category = "望遠レンズ";
+	        else category = "標準レンズ";
+
+	        return category;
+        }
+
+
+        /**
+         * @brief 画角
+         * 
+         * @param focus_length 
+         * @param distance 
+         * @return double 
+         */
+        inline double angle_of_view(double focus_length, double distance) {
+            double r2d = 180.0 / std::numbers::pi;
+            return 2 * std::atan2(0.5 * distance, focus_length) * r2d;
+        }
+
+
+        /**
+         * @brief 有効口径
+         * 
+         * @param focus_length 
+         * @param f_number 
+         * @return double 
+         */
+        inline double effective_aperture(double focus_length, double f_number) {
+            return focus_length / f_number;
+        }
+
+
+        /**
+         * @brief 被写界深度
+         * 
+         * @param permissible_circle_of_confusion 
+         * @param work_distance 
+         * @param f_number 
+         * @param focus_length 
+         * @return std::tuple<double, double, double> 
+         */
+        inline std::tuple<double, double, double>
+        depth_of_fields(double permissible_circle_of_confusion, double work_distance, 
+                        double f_number, double focus_length) 
+        {
+            double diff_len = (work_distance - focus_length);
+            double coeff = permissible_circle_of_confusion * diff_len * f_number;
+
+            // 前側被写界深度
+            double front_depth = coeff * diff_len / (focus_length * 2 + coeff);
+
+            // 後側被写界深度
+            double back_depth = coeff * diff_len / (focus_length * 2 - coeff);
+
+            // 総被写界深度
+            double total_depth = front_depth + back_depth;
+
+            return std::make_tuple(front_depth, back_depth, total_depth);
+        }
+    }
+}
